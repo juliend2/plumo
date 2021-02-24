cb63b65d7c2bcfa36bd9814003a74c93f3cc2778
Julien Desrosiers <juliend2@gmail.com>
2021-02-23 22:11:05 -0500

    finite state machine

--- plumo.js
+++ plumo.js
@@ -6,18 +6,41 @@ const PLUMO_LEAVING = document.getElementById('plumo-leaving').innerHTML;
 const Plumo = {
   init: function(container) {
     this.container = container
-    this.left()
+    this.fsm = new StateMachine({
+      init: 'none',
+      transitions: [
+        { name: 'wait', from: ['none', 'listening', 'eating'], to: 'waiting' },
+        { name: 'eat', from: ['listening', 'waiting'], to: 'eating' },
+        { name: 'listen', from: ['none', 'eating', 'waiting'], to: 'listening' },
+        { name: 'quit', from: ['listening', 'waiting', 'eating'], to: 'quitting' }
+      ],
+      methods: {
+        onEat: ()=>{
+          this.container.innerHTML = PLUMO_EATING
+        },
+        onListen: ()=>{
+          this.container.innerHTML = PLUMO_LISTENING
+        },
+        onWait: ()=>{
+          this.container.innerHTML = PLUMO_LEFT
+        },
+        onQuit: ()=>{
+          this.container.innerHTML = PLUMO_LEAVING
+        }
+      }
+    });
+    this.wait()
   },
   listen: function () {
-    this.container.innerHTML = PLUMO_LISTENING
+    this.fsm.listen();
   },
   eat: function() {
-    this.container.innerHTML = PLUMO_EATING
+    this.fsm.eat();
   },
-  left: function() {
-    this.container.innerHTML = PLUMO_LEFT
+  wait: function() {
+    this.fsm.wait();
   },
   leave: function() {
-    this.container.innerHTML = PLUMO_LEAVING
+    this.fsm.quit();
   }
 }
