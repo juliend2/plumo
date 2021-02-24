b2bc0a43cc6c0dc5084140c211e00b5f0c310b15
Julien Desrosiers <juliend2@gmail.com>
2021-02-23 21:28:46 -0500

    boutons

--- plumo.js
+++ plumo.js
@@ -1,4 +1,4 @@
-const PLUMO_ATTENTIVE = document.getElementById('plumo-attentive').innerHTML;
+const PLUMO_LISTENING = document.getElementById('plumo-listening').innerHTML;
 const PLUMO_EATING = document.getElementById('plumo-eating').innerHTML;
 const PLUMO_LEFT = document.getElementById('plumo-left').innerHTML;
 const PLUMO_LEAVING = document.getElementById('plumo-leaving').innerHTML;
@@ -6,10 +6,10 @@ const PLUMO_LEAVING = document.getElementById('plumo-leaving').innerHTML;
 const Plumo = {
   init: function(container) {
     this.container = container
-    this.wait()
+    this.left()
   },
-  wait: function () {
-    this.container.innerHTML = PLUMO_ATTENTIVE
+  listen: function () {
+    this.container.innerHTML = PLUMO_LISTENING
   },
   eat: function() {
     this.container.innerHTML = PLUMO_EATING
